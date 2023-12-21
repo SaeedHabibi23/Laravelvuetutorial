2760d98b12df53bbfc6108831ea27c677418be72
SaeedHabibi23 <ghulamsaeed54@gmail.com>
2023-12-21 20:09:20 +0430

    Admin Panel Created

--- resources/js/route.js
+++ resources/js/route.js
@@ -1,36 +1,89 @@
-import { createRouter, createWebHistory } from 'vue-router'
-import firstPage from './components/pages/newpage.vue'
-import secondPage from './components/pages/pagetwo.vue'
+import Vue from 'vue'
+import Router from 'vue-router'
+Vue.use(Router)
+import firstPage from './components/pages/myFirstVuePage'
+import newRoutePage from './components/pages/newRoutePage'
 import hooks from './components/pages/basic/hooks.vue'
 import methods from './components/pages/basic/methods.vue'
 
-export const routes = [
-  {
-    name: 'home' , 
-    path:'/' , 
-    component: firstPage , 
-   
-  },
-  {
-    name: 'secondpage' , 
-    path:'/secondpage' , 
-    component: secondPage
-  },
-  {
-    name: 'hooks' , 
-    path:'/hooks' , 
-    component: hooks
-  },
-  {
-    name: 'methods' , 
-    path:'/methods' , 
-    component: methods
-  },
+// project pages 
+import home from './components/pages/home'
+import tags from './components/pages/tags'
+
+
+const routes = [
+    //projects routes....
+
+    {
+        path: '/', 
+        component: home, 
+       
+    },
+    {
+        path: '/tags', 
+        component: tags, 
+       
+    },
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    
+
+
+    {
+        path: '/my-new-vue-route', 
+        component: firstPage, 
+       
+    },
+
+
+    /// basic tutorials routes... 
+    {
+        path: '/my-new-vue-route', 
+        component: firstPage, 
+       
+    },
+    {
+        path: '/new-route', 
+        component: newRoutePage
+    },
+
+    // vue hooks 
+    {
+        path: '/hooks', 
+        component: hooks
+    },
+    // more basics 
+    {
+        path: '/methods', 
+        component: methods
+    },
+
+
 ]
 
-const router = createRouter({
-  history: createWebHistory() ,
-  routes: routes
-})
 
-export default router
+
+export default new Router({
+    mode: 'history', 
+    routes
+
+})
